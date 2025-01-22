06ec09b55caf1db4c92256421ccc9bf7bfcf70b1
David Fernández <davidfernandezcomesana@gmail.com>
2025-01-22 22:05:16 +0100

    test added

---
+++ index.test.js
@@ -0,0 +1,19 @@
+const TinyDateUtils = require("../src/index");
+
+test("formatDate works correctly", () => {
+  const date = new Date("2025-01-22");
+  expect(TinyDateUtils.formatDate(date)).toBe("2025-01-22");
+  expect(TinyDateUtils.formatDate(date, "MM/DD/YYYY")).toBe("01/22/2025");
+});
+
+test("addDays works correctly", () => {
+  const date = new Date("2025-01-22");
+  const newDate = TinyDateUtils.addDays(date, 5);
+  expect(TinyDateUtils.formatDate(newDate)).toBe("2025-01-27");
+});
+
+test("differenceInDays works correctly", () => {
+  const date1 = new Date("2025-01-22");
+  const date2 = new Date("2025-01-27");
+  expect(TinyDateUtils.differenceInDays(date1, date2)).toBe(5);
+});
